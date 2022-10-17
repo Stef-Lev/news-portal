f976f9af73d786d142ed63363c14e5d1d7989522
Stefanos Leventis <sigianas@yahoo.com>
2022-10-17 23:22:21 +0300

    updated weather and news api

--- pages/api/news.ts
+++ pages/api/news.ts
@@ -14,6 +14,28 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
   ) {
+    let news = {}
+    const categories = ['Σπορ', 'ygeiamou.gr', 'Κόσμος', 'People', 'Ελλάδα', 'Πολιτική', 'Οικονομία', 'Πολιτισμός']
+    const tabs = {
+      'Σπορ':'Αθλητισμός', 
+      'ygeiamou.gr': 'Υγεία', 
+      'Κόσμος':'Κόσμος', 
+      'People':'Lifestyle', 
+      'Ελλάδα':'Ελλάδα', 
+      'Πολιτική':'Πολιτική', 
+      'Οικονομία':'Οικονομία', 
+      'Πολιτισμός':'Πολιτισμός'}
     let feed = await parser.parseURL(NEWS_URL);
-    res.status(200).json(feed);
+    feed.items.map(item => {
+      if (categories.includes(item.categories[0])){
+        if (news[tabs[item.categories[0]]]) {
+          news[tabs[item.categories[0]]].push(item)
+        } else {
+          news[tabs[item.categories[0]]] = []
+          news[tabs[item.categories[0]]].push(item)
+        }
+      }
+    })
+
+    res.status(200).json(news);
 }
