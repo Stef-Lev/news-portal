731b257a0bca6e4a810072e50c17d4029de7bcb3
Stefanos Leventis <sigianas@yahoo.com>
2023-01-22 16:55:35 +0200

    fix score data endpoint

--- pages/categories/scores.tsx
+++ pages/categories/scores.tsx
@@ -1,7 +1,22 @@
 import { getScores } from "../../helpers/fetchData";
+import { Image, Flex, Text, GridItem, Box } from "@chakra-ui/react";
 
 function Scores({ scores }) {
-  return <div>scores</div>;
+  console.log(Object.values(scores));
+  return (
+    <Box mt="70px">
+      {Object.values(scores)[0].map((item) => (
+        <Flex flexDirection="column" key={item.id} border="1px solid white">
+          <Box>
+            {item.teams.home} -{item.score.goal1}
+          </Box>{" "}
+          <Box>
+            {item.teams.away} -{item.score.goal2}
+          </Box>
+        </Flex>
+      ))}
+    </Box>
+  );
 }
 
 export default Scores;
