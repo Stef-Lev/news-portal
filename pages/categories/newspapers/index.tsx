2493e8acf8bb5bc2b09db9e6e8de48f93bee99c4
Stefanos Leventis <sleventis@ph.esl-asia.com>
2023-01-31 22:07:18 +0200

    add newspaper page layout

--- pages/categories/newspapers/index.tsx
+++ pages/categories/newspapers/index.tsx
@@ -1,32 +1,45 @@
 import scrapeIt from "scrape-it";
+import { useRouter } from "next/router";
+import { pathToTitle } from "../../../helpers/pathTitles";
 import type { NextPage } from "next";
 import { NextPageContext } from "next";
 import {
-  Container,
   Heading,
   Text,
   Image,
-  Center,
-  IconButton,
   Box,
-  HStack,
   Link,
+  Flex,
+  Center,
 } from "@chakra-ui/react";
 
 const Newspapers: NextPage = ({ data }) => {
+  const router = useRouter();
+  console.log(router);
   console.log(data);
   return (
     <Box mt="90px" mb="20px" borderBottom="1px solid #f3f3f3" pb="8px">
-      <Text>Newspapers</Text>
+      <Heading marginBottom="16px">ΕΦΗΜΕΡΙΔΕΣ</Heading>
 
-      {data.papers.map((item) => (
-        <Box key={item.url}>
-          <Heading>{item.title}</Heading>
-          <Link href={`/newspapers/${item.url}`}>
-            <Image src={item.img} />
-          </Link>
-        </Box>
-      ))}
+      <Flex flexWrap="wrap" gap={5} justifyContent="center">
+        {data.papers.map((item) => (
+          <Box key={item.url}>
+            <Flex
+              display="flex"
+              flexDirection="column"
+              alignItems="center"
+              w="300px"
+            >
+              <Heading as="h5" fontSize="20px">
+                {item.title}
+              </Heading>
+              <Link href={`/newspapers/${item.url}`}>
+                <Image src={item.img} alt={item.title} />
+              </Link>
+            </Flex>
+          </Box>
+        ))}
+      </Flex>
     </Box>
   );
 };
@@ -34,7 +47,7 @@ const Newspapers: NextPage = ({ data }) => {
 export default Newspapers;
 
 export async function getServerSideProps(ctx: NextPageContext) {
-  const url = "https://www.protothema.gr/frontpages/";
+  const url = process.env.PAPERS_URL;
 
   let finalData;
   const ftc = await scrapeIt(url, {
