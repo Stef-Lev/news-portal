f993695721691708772883f47436a73e15794b62
Stefanos Leventis <sigianas@yahoo.com>
2023-02-11 16:46:18 +0200

    fix metadata

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -35,7 +35,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta property="og:image" content="/preview.png" />
         <meta
           property="og:description"
-          content="Check the news from around the world in this greek news portal made with NextJS."
+          content="Read news from around the world in this greek news portal made with NextJS."
         />
       </Head>
       <Layout>
